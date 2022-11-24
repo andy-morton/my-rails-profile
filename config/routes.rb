a32636a1c4930ef10ace5cb5065bcf3ed36314dd
Andrew Morton <ammorton22@gmail.com>
2022-11-24 13:48:49 +0000

    Getting the overlay working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
   get "campcanary", to: "pages#campcanary"
+  get "learnbetter", to: "pages#learnbetter"
   # Defines the root path route ("/")
   # root "articles#index"
 end
